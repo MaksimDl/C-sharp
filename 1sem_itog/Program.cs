2a6f0bb311c4e135b6589d620e8b5cf02a6a77c5
Maksim_Dl <maksimdolgov@mail.ru>
2023-02-14 11:32:53 +0700

    add result program for task of specialization block

---
+++ 1sem_itog/Program.cs
@@ -0,0 +1,34 @@
+﻿// ## Задача:
+// Написать программу, которая из имеющегося массива стро формирует массив строк, длинна которых меньше либо 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+using static System.Console;
+Clear();
+
+//string[] strings = new string[] { "hello", "2", "world", ":-)"};
+WriteLine("Input Several strings, separated by space(s)");
+string[] strings = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] result = new string[strings.Length];
+
+int count = 0;
+
+
+for (int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        result[count] = strings[i];
+        count += 1;
+    }
+}
+
+
+result = result[..^(strings.Length - count)];   // slice excess elements of array
+
+WriteLine("Result array: [{0}]", String.Join(",", result));
+
+
+
+
+
